fb82386b6aae48b380f33e53eada778ad79f8e22
butnaruv <butnaru.valeria05@gmail.com>
2022-11-30 10:50:21 +0200

    Refactor isClocked() method from stream cipher

--- src/com/company/SymmetricCiphers/StreamCipher/Register.java
+++ src/com/company/SymmetricCiphers/StreamCipher/Register.java
@@ -98,7 +98,10 @@ public class Register {
     //Clock the register if the value of the clocking bit and the value of majority are equal
     //Clock --> means to perform xor operation for feedback bits and to shift the register one position in order to add the new computed value
     public static ArrayList<Integer> IsClocked(Integer valueOfMajority, ArrayList<Integer> reg, Integer clockingBit, ArrayList<Integer> feedbackBits) {
-
+        if(valueOfMajority == reg.get(clockingBit)){
+            int valueToAdd = FeedbackBitsXored(reg, feedbackBits);
+            reg = ShiftRegister(reg,valueToAdd );
+        }
         return reg;
     }
 
